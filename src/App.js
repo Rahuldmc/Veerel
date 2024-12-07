699de0c3852230591beeb77a00f13b0fbb6467f1
Rahuldmc <rahulprakash203@gmail.com>
2024-12-07 16:58:35 +0530

    BUG FIX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import './App.css';
-import NavBar from "./Sections/Home/NavBar"
 import Home from "./Sections/Home/Home"
-import About from "./Sections/About/About"
-import History from "./Sections/History/History"
-import Projects from './Sections/Project/Projects';
-import Contact from "./Sections/Contact/Contact"
-import Clients from "./Sections/Clients/Clients"
-import Footer from './Sections/Footer/Footer';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
-import Products from './Sections/Products/Products';
 
 function App() {
   return (
